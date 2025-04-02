dcf73aec2658fcf740d735427feca7661f41df79
Gustavo Eraso <gustavoeraso1988@gmail.com>
2025-04-01 22:47:32 -0300

    set max w to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,9 +86,9 @@ const provisoria: Animal[]= [
 ]
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
       <Header/>
-      <main className="flex flex-col w-full items-center justify-center">
+      <main className="flex flex-col w-full max-w-6xl items-center justify-center">
         <CardContainer animalsList={provisoria}/>
         
    
