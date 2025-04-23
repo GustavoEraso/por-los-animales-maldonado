0a99a38ee73d1a3f760e974ae0d407190ce849bb
Gustavo Eraso <gustavoeraso1988@gmail.com>
2025-04-23 01:21:11 -0300

    refactor: update links structure

--- src/lib/navLinks.ts
+++ src/lib/navLinks.ts
@@ -13,33 +13,35 @@ interface Links {
 }
 
 export const navLinks: Links[] = [
-    { id: 'LINKSNOSOTROS', name: 'NOSOTROS', url: '/nosotros' },
+    { id: 'LINKSINICIO', name: 'INICIO', url: '/' },
     {
-        id: 'LINKSADOPTA', name: 'ADOPTÁ', url: '/#adopta', childs: [
-            { id: 'LINKSADOPTACONOCELOS', name: 'CONOCELOS', url: '/#conocelos' },
-            { id: 'LINKSADOPTAQUIEROADOTAR', name: 'QUIERO ADOPTAR', url: '/#quiero-adoptar' },
-            { id: 'LINKSADOPTAREQUISITOSDEADOPCION', name: 'REQUISOTOS DE ADOPCIÓN', url: '/#requisitos-de-adopcion' },
-        ]
+        id: 'LINKSADOPTA', name: 'ADOPTÁ', url: '/adopta', 
+        // childs: [
+        //     { id: 'LINKSADOPTACONOCELOS', name: 'CONOCELOS', url: '/#conocelos' },
+        //     { id: 'LINKSADOPTAQUIEROADOTAR', name: 'QUIERO ADOPTAR', url: '/#quiero-adoptar' },
+        //     { id: 'LINKSADOPTAREQUISITOSDEADOPCION', name: 'REQUISOTOS DE ADOPCIÓN', url: '/#requisitos-de-adopcion' },
+        // ]
     },
     {
-        id: 'LINKSSALVAVIDAS', name: 'SALVÁ VIDAS', url: '/#salva-vidas', childs: [
-            { id: 'LINKSSALVAVIDASCASTA', name: 'CASTRÁ', url: '/#castrá' },
-            { id: 'LINKSSALVAVIDASDENICIAMALTRATO', name: 'DENINCIA EL MALTRATO', url: '/#denuncia-el-maltrato' },
-        ]
-    },
-    {
-        id: 'LINKSPARTICIPA', name: 'PARTICIPÁ', url: '/#cparticipa', childs: [
-            { id: 'LINKSPARTICIPAVOLUNTARIOS', name: 'VOLUNTARIOS', url: '/#sobre-nosotros' },
-            { id: 'LINKSPARTICIPAHOGARDETRANSITO', name: 'HOGAR DE TRÁNSITO', url: '/#hogar-de-transito' },
-
-        ]
-    },
-    { id: 'LINKSTIENDA', name: 'TIENDA', url: '/#tienda' },
-    {
-        id: 'LINKSDONACIONES', name: 'DONACIONES', url: '/donaciones', childs: [
+        id: 'LINKSDONA', name: 'DONA', url: '/donaciones', childs: [
             { id: 'LINKSDONACIONESAPORTESECONOMICOS', name: 'APORTES ECONÓMICOS', url: '/donaciones#aportes-economicos' },
             { id: 'LINKSDONACIONESDONACIONDEINSUMOS', name: 'DONACÓN DE INSUMOS', url: '/donaciones#donacion-de-insumos' },]
     },
-    { id: 'LINKSCONTACTO', name: 'CONTACTO', url: '/#contacto' },
+    {
+        id: 'LINKSINVOLUCRATE', name: 'INVOLUCRATE', url: '/involucrate', childs: [
+            { id: 'LINKSINVOLUCRATETRANSITORIO', name: 'TRANSITORIO', url: '/involucrate#transitorio' },
+            { id: 'LINKSINVOLUCRATECASTRACIONES', name: 'CASTRACIONES', url: '/involucrate#castraciones' },
+            { id: 'LINKSINVOLUCRATEOTRASFORMASDECOLABORAR', name: 'OTRAS FORMAS DE COLABORAR', url: '/involucrate#otras-formas-de-colaborar' },
+            { id: 'LINKSINVOLUCRATEDIFUSION', name: 'DIFUSION', url: '/involucrate#difusion' },
+            { id: 'LINKSINVOLUCRATEDENUNCIA', name: 'DENUNCIA MALTRATO', url: '/involucrate#denuncia-maltrato' },
+            
+
+        ]
+    },
+    { id: 'LINKSNOSOTROS', name: 'NOSOTROS', url: '/nosotros', childs: [
+        { id: 'LINKSNOSOTROSQUIENESSOMOS', name: 'QUIENES SOMOS', url: '/nosotros#quienes-somos' },
+        { id: 'LINKSNOSOTROSREDES', name: 'REDES SOCIALES', url: '/nosotros#redes-sociales' },
+        { id: 'LINKSNOSOTROSEMAIL', name: 'EMAIL', url: '/nosotros#email' },
+    ] },
 ]
 
