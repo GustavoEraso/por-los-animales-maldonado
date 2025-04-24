329e8949bcb8041aa8ed3dd905a104c564286177
Gustavo Eraso <gustavoeraso1988@gmail.com>
2025-04-24 04:49:41 -0300

    feat: cerate /involucrate & /adopta pages

---
+++ src/app/adopta/page.tsx
@@ -0,0 +1,16 @@
+
+import Hero from "@/components/Hero";
+
+export default function Nosotros() {
+  return (
+    <div className="flex flex-col items-center gap-8 w-full min-h-screen bg-white">
+
+      <Hero title="en construccion" />
+      <section className="flex flex-col lg:flex-row gap-4 px-9 py-4 w-full  max-w-7xl justify-center items-center">
+        
+      </section>
+
+
+    </div>
+  );
+}
