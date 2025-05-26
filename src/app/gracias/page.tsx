b891ac9bf68a4a8b53f0419fd944d21e0fb50ef0
Gustavo Eraso <gustavoeraso1988@gmail.com>
2025-05-25 23:48:45 -0300

    fix: add Suspense to gracias page

--- src/app/gracias/page.tsx
+++ src/app/gracias/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import IconCard from "@/components/IconCard";
 
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from "react";
 
 export default function Nosotros() {
 const searchParams = useSearchParams();
@@ -24,6 +25,7 @@ const contributionType: Record <string, string> = {
       <Hero imgURL="/madre-con-cachorros.webp" imgAlt="cachorros jugando mientras su madre mira desde atras" />
       <section className="flex flex-col  gap-4 px-9 py-4 w-full  max-w-7xl justify-center items-center">
         <section className=" w-full ">
+             <Suspense fallback={<div>Cargando...</div>}>
 
             <h1 className="text-3xl lg:text-5xl font-bold text-center text-balance  text-black">¡Gracias <span className="capitalize">{name}</span>!</h1>
             <p className="text-lg lg:text-xl text-center text-balance text-gray-700">
@@ -32,6 +34,7 @@ const contributionType: Record <string, string> = {
             <p className="text-lg lg:text-xl text-center text-balance text-gray-700">
                 Con tu ayuda, podemos seguir brindando atención y amor a nuestros animales rescatados.
             </p>
+            </Suspense>
             <div className="flex flex-col lg:flex-row gap-4 justify-center items-center mt-4">
                 <IconCard
                 icon="/logo300.webp"
