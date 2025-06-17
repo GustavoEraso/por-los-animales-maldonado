b7ddb87e073d948cc011e41f67f6a52ffd7848f8
Gustavo Eraso <gustavoeraso1988@gmail.com>
2025-06-17 20:47:41 -0300

    feat: add suspense to login page

---
+++ src/app/login/LoginClient.tsx
@@ -0,0 +1,22 @@
+'use client';
+import { useSearchParams } from "next/navigation";
+import Loginform from "@/components/LoginForm";
+export default function LoginClient() {
+  const searchParams = useSearchParams();
+  const error = searchParams.get("error");
+  const errorMessage = error === "unauthorized" ? "No estás autorizado para acceder a esta página." : "";
+
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen p-4">
+      {errorMessage && (
+        <div className="bg-red-100 text-red-800 p-4 rounded-md mb-4">
+          <p>{errorMessage}</p>
+        </div>)}
+      <Loginform />
+      {errorMessage && (
+        <div className="bg-red-100 text-red-800 p-4 rounded-md mb-4">
+          <p>{errorMessage}</p>
+        </div>)}
+    </main>
+  );
+}
