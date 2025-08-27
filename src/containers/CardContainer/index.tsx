348d446628176f902122087037c338a40fbad77c
Gustavo Eraso <gustavoeraso1988@gmail.com>
2025-08-27 17:09:36 -0300

    fix: change auto-fit to auto-fill in grid layout

--- src/containers/CardContainer/index.tsx
+++ src/containers/CardContainer/index.tsx
@@ -5,7 +5,7 @@ import { Animal } from "@/types";
 
 export default function CardContainer({animalsList=[]}:{animalsList:Animal[]}) {
   return (
-    <div className="grid grid-cols-[repeat(auto-fit,minmax(240px,1fr))] gap-4 sm:p-4">
+    <div className="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-4 sm:p-4">
       {
         animalsList.map((animal, index)=><Card key={`${animal.id}CardContainer${index}`} props={animal} />)
       }          
