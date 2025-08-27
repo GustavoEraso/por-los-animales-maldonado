a60a9459afb3761aa56fbe28e9353d9e4389da75
Gustavo Eraso <gustavoeraso1988@gmail.com>
2025-08-27 16:30:18 -0300

    fix: ui

--- src/app/(main)/adopta/[id]/page.tsx
+++ src/app/(main)/adopta/[id]/page.tsx
@@ -45,7 +45,7 @@ export default async function AnimalPage({ params }: PageProps) {
       </ViewTransition>
 
       <section className="flex flex-col lg:flex-row gap-4 py-4 w-full justify-center items-center">
-        <div className="flex flex-col md:flex-row gap-4 px-9 py-4 max-w-7xl">
+        <div className="flex flex-col md:flex-row gap-4 px-9 py-4 w-full max-w-7xl">
           <div className="flex flex-col gap-4 text-start text-black px-2 md:w-3/5">
             <p className="text-green-dark text-lg font-bold">{description}</p>
             <ul className="list-disc pl-4 text-green-dark">
